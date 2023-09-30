36189feb42a79d39f41df9a0db4d236b906ca0e4
andreachirico11 <andreachirico11@gmail.com>
2023-09-30 09:27:36 +0200

    env + sqlize + db test

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
-const express = require('express')
-const app = express()
+if (!process.env.NODE_ENV || process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+const port = process.env.PORT || 3000;
+const dbUri = process.env.DB_URI || '';
+
+const express = require('express');
+const app = express();
+
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize(dbUri, {
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: true,
+    },
+  },
+});
+
+const PingTests = sequelize.define('pingTest', {
+  name: {
+    type: DataTypes.STRING,
+  },
+});
+
 app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-const port = process.env.PORT || 3000
-app.listen(port, () => {
-    console.log("listening on port: " + port);
-})
+  PingTests.findAll({ attributes: ['name'] }).then((tests) => {
+    res.send(tests.length ? tests[0].name : 'no tests');
+  });
+});
+
+sequelize.authenticate().then(() => {
+  app.listen(port, () => {
+    console.log(
+      'version -------------> ' + (process.env.PRODUCTION === '1' ? 'PRODUCTION' : 'DEVELOPMENT')
+    );
+    console.log('listening on port: ' + port);
+  });
+});
